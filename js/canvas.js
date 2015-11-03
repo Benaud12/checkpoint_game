cb095eccd317adcc651449cd09910eb2de9feb97
Proskurina <yproskurina@yahoo.com>
2015-11-03 17:57:06 +0000

    Line between 2 points, zoom does not work, as svg layer is on top, when zoom works line does not follow the points, canvas can be on top, same issues

--- js/canvas.js
+++ js/canvas.js
@@ -1,19 +1,19 @@
-var canvas = document.getElementById("map_canvas");
-var ctx = canvas.getContext("2d");
+// var canvas = document.getElementById("map_canvas");
+// var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.moveTo(50,20);
-ctx.bezierCurveTo(140, 100, 100, 130, 240, 30);
-ctx.stroke();
+// ctx.beginPath();
+// ctx.moveTo(50,20);
+// ctx.bezierCurveTo(140, 100, 100, 130, 240, 30);
+// ctx.stroke();
 
-ctx.fillStyle = 'blue';
-// start point
-ctx.fillRect(50, 20, 10, 10);
-// end point
-ctx.fillRect(240, 30, 10, 10);
+// ctx.fillStyle = 'blue';
+// // start point
+// ctx.fillRect(50, 20, 10, 10);
+// // end point
+// ctx.fillRect(240, 30, 10, 10);
 
-ctx.fillStyle = 'red';
-// control point one
-ctx.fillRect(140, 100, 10, 10);
-// control point two
-ctx.fillRect(100, 130, 10, 10);
+// ctx.fillStyle = 'red';
+// // control point one
+// ctx.fillRect(140, 100, 10, 10);
+// // control point two
+// ctx.fillRect(100, 130, 10, 10);
--- js/map.js
+++ js/map.js
@@ -1,3 +1,8 @@
+var markerArray = [
+  {latLng: [39.90, 12.45], name: 'Vatican City'},
+  {latLng: [43.73, 7.41], name: 'Monaco'}
+]
+
 $(function(){
   $('#europe').vectorMap({
     map: 'europe_mill',
@@ -12,9 +17,13 @@ $(function(){
       }
     },
     backgroundColor: '#383f47',
-    markers: [
-      {latLng: [39.90, 12.45], name: 'Vatican City'},
-      {latLng: [43.73, 7.41], name: 'Monaco'}
-    ]
+    markers: markerArray
   });
+
+var map = $('#europe').vectorMap('get', 'mapObject');
+var draw = SVG('svgMapOverlay').size(660, 400);
+var coords1 = map.latLngToPoint(markerArray[0].latLng[0],markerArray[0].latLng[1]);
+var coords2 = map.latLngToPoint(markerArray[1].latLng[0],markerArray[1].latLng[1]);
+draw.path().attr({ fill: 'none',stroke: '#c00', 'stroke-width': 2 }).M(coords1.x, coords1.y).L(coords2.x, coords2.y);
+
 });
