8f9b26845fa91183359e461fa412a9bc8d4d09d0
Proskurina <yproskurina@yahoo.com>
2015-11-03 13:38:30 +0000

    Initial commit, tabs are clickable

---
+++ js/tabController.js
@@ -0,0 +1,14 @@
+gameApp.controller("tabCtrl", [function(){
+  var self = this;
+
+  self.tab = 'map'
+
+  this.selectTab = function(setTab){
+    self.tab = setTab;
+  };
+
+  this.isSelected = function(checkTab){
+    return self.tab === checkTab;
+  };
+
+}])
