9f8f842bae26ac1f09f6c82aebed45460f5d5565
Proskurina <yproskurina@yahoo.com>
2015-11-03 18:16:57 +0000

    stuff

--- js/canvas.js
+++ js/canvas.js
@@ -1,19 +1,21 @@
-// var canvas = document.getElementById("map_canvas");
-// var ctx = canvas.getContext("2d");
+var canvas = document.getElementById("map_canvas");
+var context = canvas.getContext("2d");
 
-// ctx.beginPath();
-// ctx.moveTo(50,20);
-// ctx.bezierCurveTo(140, 100, 100, 130, 240, 30);
-// ctx.stroke();
+      context.beginPath();
+      context.moveTo(100, 20);
 
-// ctx.fillStyle = 'blue';
-// // start point
-// ctx.fillRect(50, 20, 10, 10);
-// // end point
-// ctx.fillRect(240, 30, 10, 10);
+      // line 1
+      context.lineTo(200, 160);
 
-// ctx.fillStyle = 'red';
-// // control point one
-// ctx.fillRect(140, 100, 10, 10);
-// // control point two
-// ctx.fillRect(100, 130, 10, 10);
+      // quadratic curve
+      context.quadraticCurveTo(230, 200, 250, 120);
+
+      // bezier curve
+      context.bezierCurveTo(290, -40, 300, 200, 400, 150);
+
+      // line 2
+      context.lineTo(500, 90);
+
+      context.lineWidth = 5;
+      context.strokeStyle = 'blue';
+      context.stroke();
